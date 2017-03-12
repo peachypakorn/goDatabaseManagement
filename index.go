a8201c2b90ff038d9c868831a3b0316f753cb6a2
peachypakorn <pakorn.matana@gmail.com>
2017-03-12 18:12:02 +0700

    last debug

--- index.go
+++ index.go
@@ -1,17 +1,11 @@
 package main
 
-import (
-
-	"databaseManagement/autofillDB"
-	"time"
-	"fmt"
-)
 
 func main(){
 	//databaseConnection.StartConnection();
 	//databaseConnection.DropOldDatabase("test2");
-	file := autofillDB.ReadFile("/home/peachy/gopath/src/databaseManagement/asset/store.csv")
-	store := autofillDB.UnmarshalStore(file)
-	autofillDB.FillStruct(store)
-	fmt.Print(time.Weekday(1))
+	//file := autofillDB.ReadFile("/home/peachy/gopath/src/databaseManagement/asset/store.csv")
+	//store := autofillDB.UnmarshalStore(file)
+	//autofillDB.FillStruct(store)
+	//fmt.Print(time.Weekday(1))
 }
